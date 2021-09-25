c3bbe8f0b99f01ffb5008dca66e19718ea46c3a4
Lesha <zhuravel.lesha2005@gmail.com>
2021-09-25 05:45:16 +0300

    Second day qualification success

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/pi/WRO_FE2021
-# sudo echo STARTING PROGRAMM >> ./log.txt
-# sudo -E python3 ./main.py >> ./log.txt
+sudo echo STARTING PROGRAMM >> ./log.txt
+sudo -E python3 ./main.py >> ./log.txt 2>> ./log.txt
